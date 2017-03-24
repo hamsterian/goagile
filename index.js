18977ec444b95d57a24f3230e5435e888f96d565
Wajid Poernomo <Wajid.Poernomo@monash.edu>
2017-03-24 20:48:21 +1100

    test jira endpoint

--- index.js
+++ index.js
@@ -2,11 +2,47 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var router = express.Router();
+var request = require('request');
 
-app.use(express.static('public'))
+var options = {
+  uri: 'https://hampster.atlassian.net/rest/api/2/issue',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic aGFyc2hhZ21AZ21haWwuY29tOmhhbXBzdGVyQDEyMw=='
+  },
+  method: 'POST', 
+  json: {
+    "fields": {
+       "project":
+       { 
+          "key": "HAM"
+       },	
+       "summary": "212333",
+       "description": "TEST222",
+       "issuetype": {
+          "name": "Bug"
+      }
+    }
+  }
+};
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(info);
+  }
+  else
+    console.log(response);
+}
+
+//app.use(express.static('public'))
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
+  request(options, callback);
+  res.send('Message Sent');
+
+  //res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket){
